58ea38624910d2c3580c8ec17a9290607c8aef60
Kyutae Kim <kqt99@naver.com>
2022-05-12 00:02:29 +0900

    Create BOJ_2439.java

---
+++ BOJ_2439.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import javax.swing.text.StyleConstants;
+
+
+
+public class BOJ_2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i=0; i<N; i++){
+            for(int j=1; j<N-i; j++){
+                System.out.print(" ");
+                }
+            for(int j=0; j<i+1; j++){
+                System.out.print("*");
+                }
+            System.out.println();
+            }
+
+    }
+}
+
